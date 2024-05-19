6518c7f295f1b1eb8dd08259be96372e167b94c9
guyoguyes <g.abduba43@gmail.com>
2024-05-19 23:03:21 +0300

    project init

---
+++ themes/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+  background: '#5EBC67',
+  text: '#5EBC67'
+}
+
+export const styles = {
+  text: { color: theme.text},
+  background: { backgroundColor: theme.background}
+}
